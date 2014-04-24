ef6c74f1e06731525cc6224279fb6e20f2990b37
Harry Pan <gs0622@gmail.com>
2014-04-24 15:37:43 +0800

    optimize d255

--- d255.c
+++ d255.c
@@ -1,5 +1,5 @@
-/*http://zerojudge.tw/ShowProblem?problemid=d255*/
 #include <stdio.h>
+#include <string.h>
 
 int gcd(int a, int b)
 {
@@ -9,13 +9,27 @@ int gcd(int a, int b)
 
 int main(void)
 {
-    int n, i, j, g;
-    while (scanf("%d\n", &n)==1 && n>0 && n<501) {
-        g=0;
-        for (i=1;i<n;i++) {
-            for (j=i+1;j<=n;j++) g+=gcd(i,j);
+    int n, i, j, g[501], f[501][501];
+    memset(f, 0, sizeof(f));
+    memset(g, 0, sizeof(g)); /*g[1] is 0*/
+    for (i=1;i<501;i++) {
+#if 0
+        for (j=i+1;j<=501;j++) {
+            f[i][j] = f[j][i]? f[j][i]: gcd(i,j);
+            g[j] += f[i][j];
         }
-        printf("%d\n", g);
+        g[i]+=g[i-1];
+#else
+        for (j=i+1;j<=501;j++) {
+            f[i][j] = f[i][j-1] + gcd(i,j);
+            g[j] += f[i][j];
+        }
+#endif
+    }
+    while (scanf("%d\n", &n)==1 && n && n<501) {
+        printf("%d\n", g[n]);
     }
     return 0;
 }
+
+
