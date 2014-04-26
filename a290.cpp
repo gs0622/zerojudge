4d3fb42a7b7e3a5965afca69970ac14021899731
Harry Pan <gs0622@gmail.com>
2014-04-27 04:53:00 +0800

    update a290 with dfs

--- a290.cpp
+++ a290.cpp
@@ -7,22 +7,52 @@ using namespace std;
 #define MAX 801
 int adj[MAX][MAX];
 int visit[MAX];
-int V, E;
 queue<int> q;
 
+#if 1
 void bfs(int s)
 {
-    int i, j, node;
-    memset(visit, 0, sizeof(visit));
+    int i, j;
     q.push(s);
-    while(!q.empty())
+    while (!q.empty())
     {
-        node = q.front();
+        i = q.front();
         q.pop();
-        if (visit[node]) continue;
-        visit[node] = 1;
-        for (i=0; i<MAX; i++)
-            if(adj[node][i]) q.push(i);
+        if (visit[i]) continue;
+        visit[i] = 1;
+        for (j=0; j<MAX; j++) {
+            if(adj[i][j]) q.push(j);
+        }
+    }
+}
+#else
+void bfs(int s)
+{
+    int i, j;
+    q.push(s);
+    visit[s] = 1;
+    while (!q.empty())
+    {
+        i = q.front();
+        q.pop();
+        for (j=0; j<MAX; j++) {
+            if (adj[i][j] && !visit[j]) {
+                q.push(j);
+                visit[j] = 1;
+            }
+        }
+    }
+}
+#endif
+
+void dfs(int s)
+{
+    int j;
+    visit[s] = 1;
+    for (j=0; j<MAX; j++) {
+        if (adj[s][j] && !visit[j]) {
+            dfs(j);
+        }
     }
 }
 
@@ -40,6 +70,7 @@ int main(void)
         }
         scanf("%d %d", &A, &B);
         bfs(A);
+        //dfs(A);
         puts(ans[visit[B]]);
     }
     return 0;
