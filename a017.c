2026ad7cd3f93e9c84495c112c8c908d67d526ab
Harry Pan <gs0622@gmail.com>
2014-05-10 17:42:26 +0800

    fixed two bugs, postfix without terminator, operand order incorrect

--- a017.c
+++ a017.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 static int priority(char op) { 
     switch (op) {
@@ -11,33 +12,33 @@ static int priority(char op) {
 }
 
 void rpn(char *in, char *out) { /*Reverse Polish Notation, a.k.a infix to postfix*/
-    char stack[1024] = {};
-    int i, j, top;
-    for (i=0,j=0,top=0; in[i]!='\0'; i++)
-    switch (in[i]) {
+    char postfix[1024] = {};
+    int ii, oo, pp;
+    for (ii=0,oo=0,pp=0; in[ii]!='\0';)
+    switch (in[ii]) {
         case '(':
-            stack[++top] = in[i]; break;
+            postfix[++pp] = in[ii++]; break;
         case '+': case '-': case '*': case '/':  case '%':
-            while (priority(stack[top]) >= priority(in[i])) out[j++] = stack[top--];
-            stack[++top] = in[i];
+            while (priority(postfix[pp]) >= priority(in[ii])) out[oo++] = postfix[pp--];
+            postfix[++pp] = in[ii++];
             break;
         case ')':
-            while (stack[top] != '(') out[j++] = stack[top--];
-            top--;
+            while (postfix[pp] != '(') out[oo++] = postfix[pp--];
+            pp--,ii++;
             break;
-        default: out[j++] = in[i];
+        default: out[oo++] = in[ii++];
     }
-    while (top > 0) out[j++] = stack[top--];
+    while (pp > 0) out[oo++] = postfix[pp--];
+    out[oo]='\0'; /*null-terminator*/
 }
 
 static int calculate(char *s)
 {
-    char operand[32];
-    int i, stack[64], top=-1;
+    int i, stack[1024], top=-1;
     for (i=0;i<strlen(s);) {
         switch (s[i]) {
         case '+': case '-': case '*': case '/':  case '%': {
-                int a=stack[top--], b=stack[top--];
+                int b=stack[top--], a=stack[top--]; /*operand in stack order*/
                 if ('+'==s[i]) stack[++top]=a+b;
                 else if ('-'==s[i]) stack[++top]=a-b;
                 else if ('*'==s[i]) stack[++top]=a*b;
@@ -48,15 +49,16 @@ static int calculate(char *s)
             }
         case '0': case '1': case '2': case '3': case '4':
         case '5': case '6': case '7': case '8': case '9': {
+                char operand[64]={};
                 int len=0;
-                while (s[i]>='0'&&s[i]<='9'&&s[i]!=' '&& s[i]) operand[len++]=s[i++];
+                while (isdigit((int)s[i])) operand[len++]=s[i++];
                 stack[++top] = atoi(operand);
                 break;
             }
         default: i++; break;
         }
     }
-    return stack[top];
+    return stack[0];
 }
 
 int main(void)
@@ -67,6 +69,7 @@ int main(void)
 #endif
     while (gets(infix)) {
         rpn(infix, postfix);
+        /*puts(postfix);*/
         printf("%d\n", calculate(postfix));
     }
     return 0;
