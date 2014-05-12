50571def3ed5a34cff3896ca3957e22d34d3f993
Harry Pan <gs0622@gmail.com>
2014-05-12 19:32:34 +0800

    cut tailing space, no functional change

--- c061.c
+++ c061.c
@@ -8,7 +8,7 @@ int main(void)
     freopen("c061.in", "r", stdin);
 #endif
     while (scanf("%d %d", &n, &m)==2 && n+m /*as n>0&&m>0*/ && n>=m) {
-        if (m>n/2) m=n-m;/*Cnm = Cn(n-m)*/ 
+        if (m>n/2) m=n-m;/*Cnm = Cn(n-m)*/
         for (i=0,c=1; i<m; i++) c*=n-i, c/=(1+i); /*reduced common (n-m)!*/
         printf("%lld\n", c);
     }
