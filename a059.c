859418bc63c8741b6b367a24042c891e304ee201
Harry Pan <gs0622@gmail.com>
2014-05-13 18:40:19 +0800

    add a059

---
+++ a059.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+#define START(i,a)  (i=(int)ceil(sqrt((double)a)))
+#define END(i,b)    (i<=(int)floor(sqrt((double)b)))
+
+int main(void)
+{
+    int i, t, tt, a, b, s;
+#if 0
+    freopen("a147.in", "r", stdin);
+#endif
+    while (scanf("%d\n", &t)==1 && t) {
+        tt = t;
+        while (t--) {
+            scanf("%d\n%d", &a, &b); 
+            for (START(i,a), s=0; END(i,b);i++)  s+=i*i;
+            printf("Case %d: %d\n", tt-t, s);
+        }
+    }
+    return 0;
+}
